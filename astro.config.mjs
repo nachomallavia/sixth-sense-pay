55026047dcec4c6d17f5102f2034ecc0a5866cdc
nachomallavia <nachomallavia@hotmail.com>
2026-01-22 13:20:56 -0300

    Fix locales y escudo

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,8 +11,6 @@ import react from "@astrojs/react";
 // Carga las variables de .env.local
 dotenv.config();
 
-console.log("REDIS_URL:", process.env.TEST_VAR);
-
 // https://astro.build/config
 export default defineConfig({
     vite: {
@@ -21,20 +19,6 @@ export default defineConfig({
     output: "server",
 
     adapter: vercel(),
-    session: {
-        driver: "redis",
-        options: {
-          url: process.env.REDIS_URL,
-          maxRetriesPerRequest: 1
-        }
-      },
-    // session: {
-    //     driver: process.env.REDIS_URL ? "redis" : "memory",
-    //     options: process.env.REDIS_URL ? {
-    //       url: process.env.REDIS_URL,
-    //       maxRetriesPerRequest: 1
-    //     } : {}
-    //   },
     integrations: [db(), react()],
     i18n: {
         locales: ["en", "es"],
--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,4 @@
 import { defineAction } from 'astro:actions';
-import type { Astro } from 'astro';
 import { db, ContactFormSubmission } from 'astro:db';
 import { z } from 'astro:schema';
 import { Resend } from 'resend';
@@ -29,25 +28,6 @@ export const server = {
 
         return { i18n: i18nContent[input.locale as keyof typeof i18nContent]}
     }
-  }),
-  setLocale: defineAction({
-    input: z.string(),
-    handler: async ( input: string, context: Astro.Context ) => {
-        try {
-            context.session?.set('locale', input)
-            const check = await context.session?.get('locale')
-            console.log('locale set to', input, 'and check is', check)
-        } catch (error) {
-            console.error('error setting locale', error)
-            return { success: false }
-        }    finally {
-            return { success: true }
-        }
-       
-
-
-
-    },
   }),
   // Basic contact form action
   sendContactForm: defineAction({
